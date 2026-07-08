2760dc02d490031136fd3ef95b57530c978e2d9a
Matias Ferrer <ferrermatias2498@gmail.com>
2026-07-08 19:51:00 -0300

    Inicio del proyecto StockFlow

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>StockFlow</h1>
+      <p>Sistema de gestión de stock</p>
+    </div>
+  )
+}
+
+export default App
